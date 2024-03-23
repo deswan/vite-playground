f46451431af6d18d705f2087d83a47408910e3f4
xingziwang <xingziwang@futunn.com>
2024-03-23 18:27:19 +0800

    coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), codecovVitePlugin({
+    enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+    bundleName: "vite-playground",
+    uploadToken: process.env.CODECOV_TOKEN,
+  }),],
 })
